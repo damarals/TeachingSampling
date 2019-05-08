9135ef8592f03290cf6633e847deb3cd569ea8ff
Daniel Amaral <silva.daniel86@Gmail.com>
2019-05-07 23:15:01 -0300

    Update E.CI.r

--- R/E.CI.r
+++ R/E.CI.r
@@ -2,31 +2,40 @@
 
 E.CI <- function (N, est, type = "total", level = 0.95) 
 {
-  if (length(dim(est)) > 2){
-    names<-dimnames(est)[[3]][-1]
-    result = NULL
-    namerow = NULL
-    for(i in 1:length(names)){
-      namerow[i] = paste0(names[i], ' ', type)
-      result = rbind(result, E.CI(N,  a[,,i+1], type = type, level = level))
+  if(length(dim(est)) == 2){
+    dim_n <- colnames(est)[-1]
+    est <- est[,-1]
+    dim(est) <- c(4, 1, ncol(est))
+    if(length(dim(est)) == 2){
+      dim(est) <- c(4, 1, ncol(est))
     }
-    rownames(result)<-namerow
-    return(result)
+    colnames(est) <- 'Population'
+    dimnames(est)[[3]] <- dim_n
   }
   else{
-    a <- as.numeric(tail(est[1,],1))
-    b <- as.numeric(tail(est[2,],1))
-    if (type == "mean") {
-      a <- as.numeric(tail(est[1,],1))/N
-      b <- as.numeric(tail(est[2,],1))/N
-    }
-    alpha <- 1 - level
-    quantil <- qnorm(1 - (alpha/2))
-    c <- quantil * b
-    Li <- a - c
-    Ls <- a + c
-    interval = matrix(c(Li,Ls),1)
-    colnames(interval) = c('ci.low', 'ci.hi')
-    return(interval)
+    est <- est[,,-1]
   }
+  result <- array(NA, c(dim(est)[2], 2, dim(est)[3]))
+  t_col <-  length(colnames(est))
+  rownames(result) <- as.vector(mapply(paste0,colnames(est),rep(' ', t_col), rep(type, t_col)))
+  colnames(result) <- c('ci.low', 'ci.hi')
+  dimnames(result)[[3]] <- dimnames(est)[[3]]
+  if(length(N) == (ncol(est)-1)){
+    N <- c(N, sum(N))
+  }
+  for(j in 1:dim(result)[3]){
+    for(k in 1:dim(result)[1]){
+      values <- est[,k,j]
+      a <- values[1]
+      alpha <- 1-level
+      b <- values[2]*qnorm(1-(alpha/2))
+      if(type == 'mean'){
+        result[k,,j] <- c(a-b, a+b)/N[k]
+      }
+      else{
+        result[k,,j] <- c(a-b, a+b)
+      }
+    }
+  }
+  return(result)
 }
